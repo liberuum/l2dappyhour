774df961abdf26ab455506e6cd3010dc940f6944
Fernando Tiberti <fertiberti@gmail.com>
2020-09-26 01:51:53 -0300

    Display ETH balance in layer 2.

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import ethers from 'ethers'
 
 export default function Home() {
   const  [apis, setApis] = useState()
-  const [address, setAddress] = useState()
+  const [data, setData] = useState()
 
+  // load apis
   useEffect(() => {
      (async () => {
        const zksync = await import('zksync')
@@ -17,14 +18,26 @@ export default function Home() {
        const ethersSigner = ethersProvider.getSigner()
        //const ethWallet = ethers.Wallet.fromEthSigner(ethersSigner, ethersProvider)
        const syncWallet = await zksync.Wallet.fromEthSigner(ethersSigner, syncProvider)
-       const accounts = await ethersProvider.listAccounts()
-       setAddress(accounts[0])
+
        setApis({zksync, ethersProvider, ethersSigner, syncWallet})
        console.log(apis)
     })()
   }, [])
 
+  // get data using apis
+  useEffect(() => {
+    if (!apis) {
+      return
+    }
+    (async () => {
+      // Committed state is not final yet
+      const committedETHBalance = await apis.syncWallet.getBalance("ETH");
+      // Verified state is final
+      const verifiedETHBalance = await apis.syncWallet.getBalance("ETH", "verified");
 
+      setData({committedETHBalance, verifiedETHBalance})
+    })()
+  }, [apis])
 
   return (
     <div className={styles.container}>
@@ -35,7 +48,12 @@ export default function Home() {
 
       {apis ? <main className={styles.main}>
         <h3>Your Address</h3>
-        <div>{address}</div>
+        <div>{apis.syncWallet.address()}</div>
+        <h3>Balances in Layer 2</h3>
+        {data && <div>
+          ETH: {data.verifiedETHBalance.toString()}
+        </div>}
+
       </main> : 'Loading...'}
     </div>
   )
