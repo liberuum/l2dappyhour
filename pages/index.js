43c1d8fedc7b17534ad3a8fc3ea3fe6b5b2e5be2
Fernando Tiberti <fertiberti@gmail.com>
2020-09-26 00:20:27 -0300

    Connect with zksync and display your address.

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
-async function loadzkSync() {
-  const zksync = await import('zksync');
-  console.log('ZkSync ', zksync);
-}
+import { useEffect, useState } from 'react'
+import ethers from 'ethers'
 
 export default function Home() {
-  loadzkSync()
+  const  [apis, setApis] = useState()
+  const [address, setAddress] = useState()
+
+  useEffect(() => {
+     (async () => {
+       const zksync = await import('zksync')
+       const syncProvider = await zksync.getDefaultProvider("mainnet")
+
+       await window.ethereum.enable()
+       const ethersProvider = new ethers.providers.Web3Provider(window.ethereum)
+       const ethersSigner = ethersProvider.getSigner()
+       //const ethWallet = ethers.Wallet.fromEthSigner(ethersSigner, ethersProvider)
+       const syncWallet = await zksync.Wallet.fromEthSigner(ethersSigner, syncProvider)
+       const accounts = await ethersProvider.listAccounts()
+       setAddress(accounts[0])
+       setApis({zksync, ethersProvider, ethersSigner, syncWallet})
+       console.log(apis)
+    })()
+  }, [])
+
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>zkSync QRCode Reader</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          zkSync QRCode Reader
-        </h1>
-
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {apis ? <main className={styles.main}>
+        <h3>Your Address</h3>
+        <div>{address}</div>
+      </main> : 'Loading...'}
     </div>
   )
 }
